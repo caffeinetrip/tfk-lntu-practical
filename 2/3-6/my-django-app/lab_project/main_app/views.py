65393ef9820caa8de88a963c0fd4aee513160100
psxks <shizenking@gmail.com>
2025-04-13 17:46:34 +0300

    lab 4

--- 2/3-6/my-django-app/lab_project/main_app/views.py
+++ 2/3-6/my-django-app/lab_project/main_app/views.py
@@ -1,3 +1,17 @@
+# main_app/views.py
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'main_app/home.html', {'title': 'Головна сторінка'})
+
+def about(request):
+    return render(request, 'main_app/about.html', {'title': 'Про нас'})
+
+def services(request):
+    return render(request, 'main_app/services.html', {'title': 'Наші послуги'})
+
+def projects(request):
+    return render(request, 'main_app/projects.html', {'title': 'Наші проекти'})
+
+def contact(request):
+    return render(request, 'main_app/contact.html', {'title': 'Зв\'яжіться з нами'})
